7cea248df211981d3be173a17069d944e48930c6
daameyaw <davidameyaw607@gmail.com>
2024-03-03 20:27:59 -0800

    src

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ export default function App() {
             />
           ) : (
             <>
-              <WatchedSummary watched={watched} />
+               <WatchedSummary watched={watched} />
               <WatchedMovieList
                 watched={watched}
                 onDeleteWatched={handleDeleteWatched}
